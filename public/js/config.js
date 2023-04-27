c5613e2bc806d67867a8a9b8f6ac44484fa459a0
master <info.pandeyabhi31@gmail.com>
2023-04-27 19:37:44 +0530

    Added files

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    CometChatAppId: '236797515e56b30f',
+    CometChatRegion: 'eu',
+      CometChatAuthKey: '9a61c2a89c7aec46ccb2e8e208f4f2acb7cb4113',
+    CometChatAPIKey: '0c6961421e0a92d79b6399644cd93a4b9138cdad'
+  };
